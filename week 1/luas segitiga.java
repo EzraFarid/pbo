21b92d9a1a8838426388d09dbb2b74c9a6b00d88
EzraFarid <90606048+EzraFarid@users.noreply.github.com>
2022-03-08 21:49:59 +0700

    Update luas segitiga.java

--- week 1/luas segitiga.java
+++ week 1/luas segitiga.java
@@ -2,24 +2,18 @@ package totod;
 import java.util.Scanner; 
 public class totod {
 	 public static void main(String[] args) {
-		//Deklarasi variabel
+		
 	       double luas;
 	       int alas, tinggi;
 
-	       //Inisialisasi objek input dari class scanner
 	       Scanner input=new Scanner(System.in);
 
-	       
-	       //Input nilai jari-jari
 	       System.out.print("Masukan Alas : ");
 	       alas= input.nextInt();
 	       System.out.print("Masukan Tinggi : ");
 	       tinggi= input.nextInt();
-
-	       //Menghitung luas lingkaran
 	       luas=alas*tinggi/2;
 
-	       //Tampilkan hasil
 	       System.out.println("Luas segitiga siku siku = "+luas);
 	    } 
 	}
